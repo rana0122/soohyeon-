7ca296793b69421dfc3088db16ee6f2846159cfe
rana0122 <rana0122@naver.com>
2024-10-31 19:15:26 +0900

    3rd

--- Dynamic/src/main/java/main.java
+++ Dynamic/src/main/java/main.java
@@ -5,6 +5,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("I'm kim sooheyon, leader commit. bye");
+		System.out.println("3rd");
 
 	}
 
